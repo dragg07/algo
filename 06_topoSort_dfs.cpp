68f20ea1c9e2b3558815c863943e3c4b1ef14f20
dragg07 <draggerr39@gmail.com>
2024-05-13 19:14:37 +0530

    Update 06_topoSort_dfs.cpp

--- 06_topoSort_dfs.cpp
+++ 06_topoSort_dfs.cpp
@@ -1,59 +1,58 @@
-#include <iostream>
-#include <vector>
-#include <stack>
+#include<iostream>
+#include<vector>
+#include<stack>
 using namespace std;
 
-//DFS traversal
-void dfs(vector<vector<int>>& graph, vector<bool>& visited, stack<int>& stk, int node) {
+void dfs(vector<vector <int>> &graph, vector<bool> &visited, stack<int> &stk, int node){
     visited[node] = true;
 
-    // Traverse all adjacent nodes
-    for (int i = 0; i < graph[node].size(); ++i) {
+    for(int i=0; i<graph[node].size(); ++i){
         int adjacent = graph[node][i];
-        if (!visited[adjacent]) {
+
+        if(!visited[adjacent])
             dfs(graph, visited, stk, adjacent);
-        }
     }
+
     stk.push(node);
 }
 
-//topological sorting
-vector<int> topologicalSort(vector<vector<int>>& graph, int n) {
-    vector<bool> visited(n, false);
+vector<int> topologicalSort(vector<vector <int>> &graph, int vertex){
+    vector<bool> visited(vertex, false);
     stack<int> stk;
 
-    // Perform DFS traversal for each unvisited node
-    for (int i = 0; i < n; ++i) {
-        if (!visited[i]) {
+    for(int i=0; i<vertex; ++i){
+        if(!visited[i])
             dfs(graph, visited, stk, i);
-        }
     }
+
     vector<int> result;
-    while (!stk.empty()) {
+    while(!stk.empty()){
         result.push_back(stk.top());
         stk.pop();
     }
+
     return result;
 }
 
-int main() {
-    int n, m;
-    cout << "Enter the number of vertices and edges: ";
-    cin >> n >> m;
+int main(){
+    int vertices, edges;
+    cout << "Enter no. of Vertices and Edges : ";
+    cin >> vertices >> edges;
 
-    vector<vector<int>> graph(n);
-    cout << "Enter the edges (u v):" << endl;
-    for (int i = 0; i < m; ++i) {
-        int u, v;
+    vector<vector <int>> graph(vertices);
+    cout << "Enter the edges (start from 0 vertex) (u v) : ";
+    for(int i=0; i<edges; ++i){
+        int u,v;
         cin >> u >> v;
         graph[u].push_back(v);
     }
-    vector<int> result = topologicalSort(graph, n);
 
-    cout << "Topological Sorting: ";
-    for (int node : result) {
+    vector<int> result = topologicalSort(graph, vertices);
+
+    cout << "\nThe Topological Sort for the following graph : " << endl;
+    for(int node : result){
         cout << node << " ";
-    }
-    cout << endl;
+    } 
+
     return 0;
 }
